8bbc9cc1041ab7a1657a7bc1cbd83ae7e11b9ca0
NickNocc <haretakis4@gmail.com>
2022-01-27 14:02:50 -0800

    model associations made

--- models/index.js
+++ models/index.js
@@ -27,7 +27,40 @@ Post.belongsToMany(User, {
 });
 
 Vote.belongsToMany(User, {
-    foreignKey:
-})
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
 
-module.exports = { User, Post, Vote, Comment }
+Vote.belongsToMany(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
