d0c36e66311ffe23ef7998618de7b61f690185cd
NickNocc <haretakis4@gmail.com>
2022-01-29 11:56:36 -0800

    api started

---
+++ public/javascript/calendarapi.js
@@ -0,0 +1,14 @@
+const { google } = require('googleapis');
+const { OAuth2 } = google.auth;
+require("dotenv").config();
+
+
+const oAuth2Client = new OAuth2(
+    process.env.Client_ID, process.env.Client_PW
+);
+
+oAuth2Client.setCredentials({refresh_token: process.env.refresh_token});
+
+const calendar = google.calendar({version: 'v3', auth: oAuth2Client });
+
+const eventStartTime = new Date()
