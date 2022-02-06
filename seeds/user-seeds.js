e9001d1963635278a64bb2897b55aba5fc89a789
NickNocc <haretakis4@gmail.com>
2022-02-06 13:07:50 -0800

    adjusted seeds to work with our dataset

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,56 +1,67 @@
 const sequelize = require('../config/connection');
 const { User, Post } = require('../models');
+const wip = 'none'
 
 const userdata = [
   {
     username: 'alesmonde0',
     email: 'nwestnedge0@cbc.ca',
-    password: 'password123'
+    password: 'password123',
+    wip: wip
   },
   {
     username: 'jwilloughway1',
     email: 'rmebes1@sogou.com',
-    password: 'password123'
+    password: 'password123',
+    wip: wip
   },
   {
     username: 'iboddam2',
     email: 'cstoneman2@last.fm',
-    password: 'password123'
+    password: 'password123',
+    wip: wip
   },
   {
     username: 'dstanmer3',
     email: 'ihellier3@goo.ne.jp',
-    password: 'password123'
+    password: 'password123',
+    wip: wip
   },
   {
     username: 'djiri4',
     email: 'gmidgley4@weather.com',
-    password: 'password123'
+    password: 'password123',
+    wip: wip
   },
   {
     username: 'msprague5',
     email: 'larnout5@imdb.com',
-    password: 'password123'
+    password: 'password123',
+    wip: wip
   },
   {
     username: 'mpergens6',
     email: 'hnapleton6@feedburner.com',
-    password: 'password123'
+    password: 'password123',
+    wip: wip
   },
   {
     username: 'tpenniell7',
     email: 'kperigo7@china.com.cn',
-    password: 'password123'
+    password: 'password123',
+    wip: wip
   },
   {
     username: 'msabbins8',
     email: 'lmongain8@google.ru',
-    password: 'password123'
+    password: 'password123',
+    wip: wip
   },
   {
     username: 'jmacarthur9',
     email: 'bsteen9@epa.gov',
-    password: 'password123'
+    password: 'password123',
+    wip: wip
   }
 ];
 
